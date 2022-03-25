02fe7a6ddcdfff90f473bf6fadefb68c2469e050
Peixoto <nuno_peixoto1997@hotmail.com>
2022-03-25 21:23:59 +0000

    Add custom scrollbar

--- resources/js/components/orders/OrdersList.vue
+++ resources/js/components/orders/OrdersList.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="bg-white shadow-sm sm:rounded-lg border-b border-gray-200">
+    <div class="bg-white shadow-sm sm:rounded-lg border-b border-gray-200 h-screen">
         <h1 class="border-b border-black text-xl font-semibold  py-4 px-4"> All orders </h1>
         <div v-if="loading" class="text-center py-2">
             <Loader></Loader>
         </div>
-        <div v-else>
-            <template v-for="order in orders" :key="order.id">
+        <div v-else class="overflow-y-auto h-full">
+            <div v-for="order in orders" :key="order.id" >
                 <article class="border-b border-gray py-4 px-4 hover:bg-gray-100 cursor-pointer" @click='selectOrder(order.id)' >
                     <p> Order #{{order.id}} </p>
                     <p> {{order.customer_first_name}} {{order.customer_last_name}} </p>
