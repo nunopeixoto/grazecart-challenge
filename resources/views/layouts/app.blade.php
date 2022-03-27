f2ea40d521dd1530e4fbe55f2aafbd480cc1420c
Peixoto <nuno_peixoto1997@hotmail.com>
2022-03-28 00:28:17 +0100

    Remove unnecessarynavigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>GrazeCart Challenge</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -18,7 +18,6 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen h-screen bg-gray-100 overflow-hidden relative" id="app">
-            @include('layouts.navigation')
 
             <!-- Page Heading -->
             <header class="bg-white shadow">
