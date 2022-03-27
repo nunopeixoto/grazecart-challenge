e3f90001003a3a751fb8d7dd1e70e90bf68fd05e
Peixoto <nuno_peixoto1997@hotmail.com>
2022-03-27 23:59:33 +0100

    Add page validation to OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,19 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Validator;
 
 class OrderController extends Controller
 {
+
     public function index(Request $request)
     {
-        $query = $request->input();
+        $validator = Validator::make($request->input(), [
+            'page' => 'numeric'
+        ]);
+
+        if ($validator->fails()) {
+            abort(422, $validator->errors());
+        }
 
         $orders = Http::withHeaders([
             'Accept' => 'application/json',
             'Authorization' => 'Bearer ' . config('grazecart.key')
         ])
         ->get('https://paramount.staging.grazecart.io/api/v1/orders', [
-            'page' => $query['page'] ?: 1
+            'page' => $validated['page'] ?? 1
         ])
         ->json();
 
